5ff3a288c51ba4580977ca6e4393bcb14e43160b
casainho <casainho@gmail.com>
2021-01-26 19:44:17 +0000

    Removed nrf52840_mdk_usb_dongle usage on EBike_wireless_remote. Updated wireless board schematic to correct small bug on GND connection

--- EBike_wireless_remote/firmware/include/pins.h
+++ EBike_wireless_remote/firmware/include/pins.h
@@ -12,27 +12,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#if defined(BOARD_CUSTOM) && defined(NRF52840_MDK_USB_DONGLE)
-// NRF52840 MakerDiary Dongle
-
-typedef enum {
-    PLUS__PIN  = 5,
-    MINUS__PIN  = 6,
-    ENTER__PIN = 7,
-    STANDBY__PIN = 8,
-    BUTTON_PIN_ELEMENTS = 4 // must be updated when added or removed an element
-} button_pins_t;
-
-typedef enum {
-    LED_G__PIN  = 0,
-    LED_R__PIN  = 1,
-    LED_B__PIN  = 2,
-} LED_pins_t;
-
-#elif defined(BOARD_PCA10059)
-
 // NRF52840 NORDIC Dongle
-
 typedef enum {
     PLUS__PIN  = 13,
     MINUS__PIN  = 15,
@@ -48,8 +28,6 @@ typedef enum {
     LED_B__PIN  = 3,
     LED_PWR__PIN = 0,
 } LED_pins_t;
-
-#endif
 
 void pins_init(void);
 bool button_plus_is_set(void);
