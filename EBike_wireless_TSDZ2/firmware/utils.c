6b8e75ba0fa73eac4fff97b2a275e76b00dfa65e
beemac <ben.machin@gmail.com>
2021-02-04 18:22:17 +0000

    Revert "working"
    This reverts commit eb581a3a67ad9bb4bbc12fb9a767e30a71865944.

--- EBike_wireless_TSDZ2/firmware/utils.c
+++ EBike_wireless_TSDZ2/firmware/utils.c
@@ -49,19 +49,16 @@ uint8_t ui8_max(uint8_t value_a, uint8_t value_b) {
  * @param[in/out]       : ui16_crc   - Anlik CRC degeri
  * @How to use          : First initial data has to be 0xFFFF.
  */
-void crc16(uint8_t ui8_data, uint16_t *ui16_crc) 
-{
-    unsigned int i;
-    uint16_t ui16_temp = *ui16_crc;
-    ui16_temp = ui16_temp ^(uint16_t) ui8_data;
-    for (i = 8; i > 0; i--)
-    {
-        if (ui16_temp & 0x0001)
-            ui16_temp = (ui16_temp >> 1) ^ 0xA001;
-        else
-            ui16_temp >>= 1;
-    }
-    *ui16_crc = ui16_temp;
+void crc16(uint8_t ui8_data, uint16_t *ui16_crc) {
+	unsigned int i;
+
+	*ui16_crc = *ui16_crc ^ (uint16_t) ui8_data;
+	for (i = 8; i > 0; i--) {
+		if (*ui16_crc & 0x0001)
+			*ui16_crc = (*ui16_crc >> 1) ^ 0xA001;
+		else
+			*ui16_crc >>= 1;
+	}
 }
 
 //// reverses a string 'str' of length 'len'
