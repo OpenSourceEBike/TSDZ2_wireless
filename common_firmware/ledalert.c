ead738fdb7f020af18e783aab6875d2b009f786a
beemac <ben.machin@gmail.com>
2021-02-18 17:45:12 +0000

    Update ledalert.c
    trying to make it sleep after led sequence

--- common_firmware/ledalert.c
+++ common_firmware/ledalert.c
@@ -190,7 +190,12 @@ void led_sequence_clock(void *p_context)
             }
         }
     }
-    else app_timer_stop(led_sequence_clock_timer); // Turn off the timer to save power
+    else 
+    {
+        app_timer_stop(led_sequence_clock_timer); // Turn off the timer to save power
+        sd_app_evt_wait(); // Try to sleep
+    }
+    
 }
 
 static void led_pwm_timer_timeout(void *p_context)
